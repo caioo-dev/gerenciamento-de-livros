86c404079058a55227ec1d4a9e8b6dcaf8e01ed5
Caio Andrade <caio.andrade2442@gmail.com>
2025-02-18 12:12:29 -0300

    first commit

---
+++ src/test/java/com/caioandrade/gerenciamento_de_livros/GerenciamentoDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caioandrade.gerenciamento_de_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
